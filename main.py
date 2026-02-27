1a09ede100a1f2d6790e135e45bf938a89e112e9
DungVu06 <vuhoangdung3103@gmail.com>
2026-02-27 07:37:21 +0700

    feat: complate logic core

---
+++ main.py
@@ -0,0 +1,70 @@
+import torch
+import yaml
+import numpy as np
+
+from PIL import Image
+from src.detection.detection_model import faster_rcnn
+from src.ocr.ocr_system import MangaTextExtractor
+from src.translation.translator_system import MangaTranslator
+from src.translation.renderer_system import MangaRenderer
+
+def load_trained_model(config_path, weights, device):
+    with open(config_path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    print(config)
+    model = faster_rcnn(
+        num_classes=config["model"]["num_classes"],
+        anchor_sizes=config["model"]["anchor_sizes"],
+        anchor_ratios=config["model"]["anchor_ratios"] 
+    )
+
+    model.load_state_dict(torch.load(weights, map_location=device, weights_only=True))
+    model.eval()
+
+    return model
+
+if __name__ == "__main__":
+    CONFIG_PATH = "./configs/faster_rcnn_default.yaml"
+    WEIGHTS_PATH = "./models/faster_rcnn_default_weights.pt"
+    INPUT_IMG_PATH = "./data/inference_data/snk_1.jpg"
+    OUTPUT_IMG_PATH = "./outputs/output.jpg"
+
+    device = "cpu"
+
+    detection_model = load_trained_model(CONFIG_PATH, WEIGHTS_PATH, device)
+    ocr_model = MangaTextExtractor()
+    translator_module = MangaTranslator("ja", "en")
+    renderer_module = MangaRenderer()
+
+    # Detection
+    confidence_threshold = 0.6
+
+    img = Image.open(INPUT_IMG_PATH).convert("RGB")
+    img_np = np.array(img)
+    img_tensor = torch.tensor(img_np / 255.0, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+    img_tensor = img_tensor.to(device)
+
+    with torch.inference_mode():
+        predictions = detection_model(img_tensor)[0]
+
+    keep_idx = predictions["scores"] >= confidence_threshold
+    boxes = predictions["boxes"][keep_idx].cpu().numpy()
+    labels = predictions["labels"][keep_idx].cpu().numpy()
+
+    text_boxes = []
+    frame_boxes = []
+
+    for box, label in zip(boxes, labels):
+        if label == 1:
+            frame_boxes.append(box)
+        else:
+            text_boxes.append(box)
+
+    # OCR 
+    ocr_results = ocr_model.extract_text(INPUT_IMG_PATH, text_boxes, frame_boxes)
+
+    # Translation
+    translated_data = translator_module.translate_with_context(ocr_results)
+
+    # Render
+    renderer_module.render_translated_image(INPUT_IMG_PATH, translated_data, OUTPUT_IMG_PATH)
---
+++ src/translation/renderer_system.py
@@ -0,0 +1,61 @@
+import textwrap
+import os
+
+from PIL import Image, ImageDraw, ImageFont
+
+class MangaRenderer:
+    def __init__(self):
+        pass
+
+    def _draw_text_centered(self, draw, text, box, max_font_size=40):
+        xmin, ymin, xmax, ymax = box
+        box_width = xmax - xmin
+        box_height = ymax - ymin
+
+        font_size = max_font_size
+        
+        while font_size > 8:
+            try:
+                font = ImageFont.load_default(size=font_size)
+            except TypeError:
+                font = ImageFont.load_default()
+
+            avg_char_width = font.getlength("a") if hasattr(font, 'getlength') else font_size * 0.5
+            max_chars_per_line = max(1, int((box_width * 0.9) / avg_char_width))
+
+            lines = textwrap.wrap(text, width=max_chars_per_line)
+
+            line_spacing = 4
+            total_text_height = len(lines) * font_size + (len(lines) - 1) * line_spacing
+
+            if total_text_height <= (box_height * 0.9):
+                break
+            
+            font_size -= 2
+
+        current_y = ymin + (box_height - total_text_height) / 2
+
+        for line in lines:
+            line_width = font.getlength(line) if hasattr(font, 'getlength') else len(line) * font_size * 0.5
+            current_x = xmin + (box_width - line_width) / 2
+            
+            draw.text((current_x, current_y), line, fill="black", font=font)
+            current_y += font_size + line_spacing
+
+    def render_translated_image(self, image_path, translated_data, output_path="final_translated_manga.jpg"):
+        img = Image.open(image_path).convert("RGB")
+        draw = ImageDraw.Draw(img)
+
+        for item in translated_data:
+            text_en = item.get("english_text", "")
+            if not text_en:
+                continue
+
+            xmin, ymin, xmax, ymax = map(int, item["coordinates"])
+
+            draw.rectangle([xmin, ymin, xmax, ymax], fill="white")
+            self._draw_text_centered(draw, text_en, [xmin, ymin, xmax, ymax])
+
+        img.save(output_path, quality=95)
+        
+        img.show()
